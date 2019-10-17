c375231b0281c6cd7c415780e8842f59e190ccf2
WillMatthews <will.a.matthews@me.com>
2019-10-17 12:24:05 +0100

    working for 2d

--- fluorotrace.py
+++ fluorotrace.py
@@ -6,7 +6,7 @@ from shapely.geometry import Point, LineString
 from shapely.geometry.polygon import Polygon
 import matplotlib.pyplot as plt
 from matplotlib import cm
-
+import random
 
 def get_stage(shape="rectangle"):
     print("Building Stage")
@@ -16,7 +16,17 @@ def get_stage(shape="rectangle"):
     elif shape == "angled":
         nodes   = [[1,0], [0,1], [1,2], [3,2], [3,0]]
         edge_types = [ "m",  "m",  "m", "d",  "m"]
-
+    elif shape == "semicircle":
+        nodes = [[-np.sin(np.pi * x), np.cos(np.pi * x)] for x in np.linspace(0,1,20)]
+        edge_types = ["m" for x in nodes]
+        edge_types[-1] = "d"
+        edge_types.append("m")
+    elif shape == "triangle1":
+        nodes = [[1,0], [0,1], [1,2]]
+        edge_types = ["m", "m", "d"]
+    elif shape == "triangle2":
+        nodes = [[1,0], [0,1], [1,2]]
+        edge_types = ["m", "m", "d"]
 
 
     poly = Polygon([n for n in nodes])
@@ -43,7 +53,7 @@ def get_stage(shape="rectangle"):
         else:
             raise Exception
         print(edge1, edge2)
-        stage["edges"][i] = {"dir":np.array([dy,-dx]) , "line": LineString([edge1, edge2]) ,"type":t}
+        stage["edges"][i] = {"from":edge1, "to": edge2, "dir":np.array([dy,-dx]) , "line": LineString([edge1, edge2]) ,"type":t}
 
     return stage
 
@@ -58,13 +68,13 @@ def reflect(ray, norm):
     return normalise((np.dot(raydir,norm)) * -2 * norm + raydir)
 
 
-def sim_ray(ray, stage, dx=0.001):
+def sim_ray(ray, stage, dx=0.1):
     detected = False
     itercount = 0
     path = []
     while not detected:
-        if itercount > 100000:
-            return
+        if itercount > 10000:
+            return None, None
         itercount += 1
         oldray = copy.deepcopy(ray)
         path.append(oldray["pos"])
@@ -84,7 +94,7 @@ def sim_ray(ray, stage, dx=0.001):
                     break
             else:
                 print("NO INTERSECTION ERROR")
-                return
+                return None, None
 
             if do_reflect:
                 ray["pos"] -= ray["dir"] * dx
@@ -94,19 +104,18 @@ def sim_ray(ray, stage, dx=0.001):
                 #print("\n"*3)
             else:
                 if is_dump:
-                    return
-                print("Struck detector")
-                return path
+                    return None, None
+                #print("Struck detector")
+                return path, itercount * dx
 
 
 
-stage = get_stage(shape="angled")
+stage = get_stage(shape="semicircle")
 
 
-num_radials = 3
-phi = 0.1
+num_radials = 1000
 
-xs = np.linspace(stage["xrange"][0]+0.1,stage["xrange"][1]-0.1,10)
+xs = np.linspace(stage["xrange"][0]+0.1,stage["xrange"][1]-0.1,15)
 ys = np.linspace(stage["yrange"][0]+0.1,stage["yrange"][1]-0.1,10)
 
 tiles = []
@@ -116,38 +125,68 @@ for x in xs:
             tiles.append([x,y])
             print("Tile Added", (x,y))
 
-
+# tiles = [[0.5, 0.5]]
 
 endpoints = []
+opls = []
 plt.figure()
 for j, tile in enumerate(tiles):
-    print("TILE", j, "/", len(tiles))
+    phi = random.random()
+    print("TILE", j+1, "/", len(tiles))
     for i in range(num_radials):
         rayObj = {"dir": normalise(np.array([np.sin(phi + 2 * np.pi * i/num_radials),np.cos(phi + 2 * np.pi * i/num_radials)])) , "pos": np.array(tile)}
-        print("RAY", i, "/", num_radials)
-        path = sim_ray(rayObj, stage)
+        #print("RAY", i+1, "/", num_radials)
+        path, opl = sim_ray(rayObj, stage)
         if path is not None:
             endpoints.append(path[-1])
+        if opl is not None:
+            opls.append(opl)
 
         if path is not None:
-            xs = [p[0] for p in path]
-            ys = [p[1] for p in path]
-            plt.plot(xs,ys)
+            pass
+            #xs = [p[0] for p in path]
+            #ys = [p[1] for p in path]
+            #plt.plot(xs,ys)
 
 plt.scatter([t[0] for t in tiles], [t[1] for t in tiles])
-plt.plot(*stage["polygon"].exterior.xy);
-plt.plot([2,2],[0,1],color="red")
+plt.plot(*stage["polygon"].exterior.xy, color="green", linewidth=2);
+for e in stage["edges"]:
+    edge = stage["edges"][e]
+    if edge["type"] == "detector":
+        plt.plot([edge["from"][0], edge["to"][0]], [edge["from"][1], edge["to"][1]], color="red", linewidth = 3)
 plt.title("Geometry")
 
 
-plt.figure()
-ends = [e[1] for e in endpoints]
+try:
+    plt.figure()
+    ends = [e[1] for e in endpoints]
 
-num_bins = 50
-# the histogram of the data
-n, bins, patches = plt.hist(ends, num_bins, density=1, facecolor='blue', alpha=0.5)
+    num_bins = 100
+    # the histogram of the data
+    n, bins, patches = plt.hist(ends, num_bins, density=1, facecolor='blue', alpha=0.5)
+    plt.xlabel("Position")
+    plt.ylabel("Freq.")
+    plt.title("Detector POSITION histogram")
+except Exception:
+    pass
 
-plt.title("detector histogram")
 
+try:
+    plt.figure()
+
+    num_bins = 100
+    # the histogram of the data
+    n, bins, patches = plt.hist(opls, num_bins, density=1, facecolor='blue', alpha=0.5)
+    plt.xlabel("OPL")
+    plt.ylabel("Freq.")
+    plt.title("Detector OPTICAL PATH LENGTH histogram")
+except Exception:
+    pass
+
+try:
+    print("MEAN OPL:", np.mean(opls))
+    print("MAX/MIN OPL:", max(opls), min(opls))
+except Exception:
+    pass
 
 plt.show()
